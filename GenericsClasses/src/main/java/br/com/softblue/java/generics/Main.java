b5accf096f2f846f7d3eda677bd39f97668d4766
samuel <samugit.projects@gmail.com>
2023-09-29 09:31:14 -0300

    Aula02.3 - Criando classes parametrizadas com o uso de generics

---
+++ GenericsClasses/src/main/java/br/com/softblue/java/generics/Main.java
@@ -0,0 +1,46 @@
+package br.com.softblue.java.generics;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//        Buffer b = new Buffer();
+//		
+//		b.adicionar("a");
+//		b.adicionar("b");
+//		b.adicionar("c");
+//
+//		String e1 = b.remover();
+//		String e2 = b.remover();
+//		String e3 = b.remover();
+//		
+//		System.out.println(e1 + ", " + e2 + ", " + e3);
+		
+		Buffer<String> b = new Buffer<>();
+		
+		b.adicionar("a");
+		b.adicionar("b");
+		b.adicionar("c");
+
+		String e1 = b.remover();
+		String e2 = b.remover();
+		String e3 = b.remover();
+		
+		System.out.println(e1 + ", " + e2 + ", " + e3);
+		
+		// podemos usar Buffer de qualquer tipo de dado pois agora é parametrizável
+		
+		Buffer<Integer> b2 = new Buffer<>();
+		
+		b2.adicionar(10);
+		b2.adicionar(15);
+		b2.adicionar(20);
+
+		int e4 = b2.remover(); // podemos parametrizar o tipo da variável como int devido o autoboxing
+		int e5 = b2.remover();
+		int e6 = b2.remover();
+		
+		System.out.println(e4 + ", " + e5 + ", " + e6);
+	}
+
+}
