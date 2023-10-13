6b99106caefd8a2c9e73a8978b3a239967367839
Samuel <samugit.projects@gmail.com>
2023-10-13 16:27:28 -0300

    Aula07.2 - Criando e executando threads

---
+++ ConcurrentThreads/src/main/java/br/com/softblue/java/concurrent/Main.java
@@ -0,0 +1,21 @@
+package br.com.softblue.java.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//ExecutorService e = Executors.newSingleThreadExecutor();
+		ExecutorService e = Executors.newFixedThreadPool(5);
+		
+		for (int i = 1; i <= 5; i++) {
+			Task t = new Task(i);
+			e.execute(t);
+		}
+		
+		e.shutdown();
+	}
+
+}
