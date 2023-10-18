f8ebd934d23ee7de3daee360fdf9106bd11723ed
Samuel <samugit.projects@gmail.com>
2023-10-18 12:33:05 -0300

    Aula07.7 - Entendendo as operações atômicas

---
+++ ConcurrentAtomic/src/main/java/br/com/softblue/java/concurrent/Gerador.java
@@ -0,0 +1,18 @@
+package br.com.softblue.java.concurrent;
+
+public class Gerador implements Runnable {
+
+	@Override
+	public void run() {
+		
+		for (int i = 0; i < 10; i++) {
+			int valor = Sequencia.next();
+			System.out.println(valor);
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
---
+++ ConcurrentAtomic/src/main/java/br/com/softblue/java/concurrent/Sequencia.java
@@ -0,0 +1,17 @@
+package br.com.softblue.java.concurrent;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Sequencia {
+	
+	//private static int currentValue = 0;
+	private static AtomicInteger currentValue = new AtomicInteger();
+	
+	public static int next() {
+		//currentValue++;
+		//return currentValue;
+		
+		return currentValue.incrementAndGet();
+	}
+
+}
