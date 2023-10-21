610de8fa5ac4e1d28b347dc2a2dc5a9e51d04eb9
Samuel <samugit.projects@gmail.com>
2023-10-20 23:41:47 -0300

    Aula08.3 - Programando sockets UDPIP em Java

---
+++ SocketUDPIP/src/main/java/br/com/softblue/java/socket/UDPServer.java
@@ -0,0 +1,25 @@
+package br.com.softblue.java.socket;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPServer {
+
+	public static void main(String[] args) throws Exception{
+		
+		try (DatagramSocket socket = new DatagramSocket(2500)) {
+			
+			byte[] buffer = new byte[1024];
+			
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			System.out.println("Servidor informa: aguardando o recebimento de datagrama...");
+			socket.receive(packet);
+			
+			byte[] clientData = packet.getData();
+			String clientDataStr = new String(clientData, 0, packet.getLength());
+			
+			System.out.println("Servidor informa: o dado recebido do cliente foi ==> " + clientDataStr);
+		}
+	}
+
+}
